901e4ca20360427c71cfc37a95f5d731211e278c
ostewartACC <olivia.stewart@g.austincc.edu>
2021-10-05 20:52:16 -0500

    hw5 code

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,63 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+double get_gc_content(const string& dna)
+{
+    double dna_length = 0;
+    double gc_count = 0;
+    double gc_content = 0;
+
+    dna_length = dna.length();
+
+    for(auto i: dna)
+    {
+        if (i == 'G' || i == 'C')
+        {
+            gc_count += 1;
+        }
+    }
+
+    gc_content = gc_count / dna_length;
+    return gc_content;
+
+}
+
+string get_dna_complement(string dna)
+{
+    string dna_comp;
+    string reverse;
+
+    reverse = reverse_string(dna);
+
+    for(auto i: reverse)
+    {
+        if (i == 'A')
+        {
+            dna_comp.push_back('T');
+        }
+        else if (i == 'T')
+        {
+            dna_comp.push_back('A');
+        }
+        else if (i == 'C')
+        {
+            dna_comp.push_back('G');
+        }
+        else if (i == 'G')
+        {
+            dna_comp.push_back('C');
+        }
+    }
+    return dna_comp;
+}
+
+string reverse_string(string dna)
+{
+    string reverse_string;
+    for (auto i: dna)
+    {
+        reverse_string = i + reverse_string;
+    }
+
+    return reverse_string;
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,2 +1,10 @@
 //add function(s) prototype here
-//git test
+#include <string>
+
+using std::string;
+
+double get_gc_content(const string& dna);
+
+string get_dna_complement(string dna);
+
+string reverse_string(string dna);
