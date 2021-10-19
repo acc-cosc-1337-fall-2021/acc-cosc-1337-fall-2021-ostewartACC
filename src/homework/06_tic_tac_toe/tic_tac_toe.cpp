74ae74faa1ee9e74868f42138e31222fafb3d1c8
ostewartACC <olivia.stewart@g.austincc.edu>
2021-10-18 21:27:36 -0500

    header and cpp outline

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,54 @@
 //cpp
+#include "tic_tac_toe.h"
+#include <iostream>
+
+using std::cout;
+
+bool TicTacToe::game_over()
+{
+    return check_board_full();
+}
+
+void TicTacToe::start_game(string first_player)
+{
+    player = first_player;
+}
+
+void TicTacToe::mark_board(int position)
+{
+    pegs[position-1] = player;
+    set_next_player();
+}
+
+string TicTacToe::get_player() const
+{
+    return player;
+}
+
+void TicTacToe::display_board() const
+{
+    cout << "BLAH\n";
+}
+
+void TicTacToe::set_next_player()
+{
+    if(player == "X")
+    {
+        player = "O";
+    }
+    else
+    {
+        player = "X";
+    }
+    
+}
+
+bool TicTacToe::check_board_full()
+{
+    return 0;
+}
+
+void TicTacToe::clear_board()
+{
+    cout << "BLAH";
+}
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,2 +1,27 @@
 //h
-//git test
+#include <string>
+#include <vector>
+
+using std::string; using std::vector;
+
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int postition);
+    string get_player() const;
+    void display_board() const;
+
+private:
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+    string player; //class member variable
+    vector<string> pegs = vector<string>(9, " "); //initialize to 9 spaces
+};
+
+#endif
